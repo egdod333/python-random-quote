11801be5f3ebc2eee6082c5e681117f632a951ea
EC2 Default User <ec2-user@ip-172-31-29-242.us-west-2.compute.internal>
2021-07-11 18:38:56 +0000

    Random quote bot is workin

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
+import random
 def primary():
-  print("Keep it logically awesome.")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  #  print("Keep it logically awesome.")
+  f = open("/home/ec2-user/environment/test/python-random-quote/quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = 13
+  rnd = random.randint(0,last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
